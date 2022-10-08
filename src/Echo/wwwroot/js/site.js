3b630e60e9415d5e030e75a041569e67c2827aeb
Janne Mattila <janne.mattila@hotmail.fi>
2022-10-08 16:48:25 +0300

    Add dark theme

--- src/Echo/wwwroot/js/site.js
+++ src/Echo/wwwroot/js/site.js
@@ -100,6 +100,23 @@ function clearLog() {
     updateTitle();
 }
 
+function changeTheme() {
+    let theme = localStorage.getItem("theme");
+    if (theme == null || theme === "light") {
+        theme = "dark";
+    }
+    else {
+        theme = "light";
+    }
+
+    localStorage.setItem("theme", theme);
+
+    document.getElementsByTagName("html")[0].className = theme;
+    document.body.classList.remove("dark");
+    document.body.classList.remove("light");
+    document.body.classList.add(theme);
+}
+
 document.addEventListener('keyup', e => {
     if (e.key === "Escape") {
         hideHelp();
@@ -116,3 +133,10 @@ document.addEventListener('keyup', e => {
         selectedElement = null;
     }
 });
+
+const startTheme = localStorage.getItem("theme");
+if (startTheme != null) {
+    console.log(startTheme);
+    document.getElementsByTagName("html")[0].className = startTheme;
+    document.body.classList.add(startTheme);
+}
